5f82c24ca404f0350fbcdb53b4b198b79a3b3a00
Ryan Morrow <ryanmorrow@Ryans-MacBook-Pro.local>
2016-06-05 10:39:17 -0700

    changed to webpack dev server

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+## 6/5/16
+* Switched server to `webpack-dev-server`
---
+++ packages/rs-httpserver/webpack.js
@@ -0,0 +1,10 @@
+const webpack = require('webpack');
+const webpackDevServer = require('webpack-dev-server');
+const config = require('../../scripts/webpack.config.js');
+
+const compiler = webpack(config);
+const server = new webpackDevServer(compiler, {
+  hot: true
+});
+
+server.listen(3000);
